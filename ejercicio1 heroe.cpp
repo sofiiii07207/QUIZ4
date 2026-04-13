8419200c747a8553d596a3cb6057deaf5a81d9f1
sofiiii07207 <vivianasofiafeijoo@hotmail.com>
2026-04-13 07:59:48 -0500

    Update ejercicio1 heroe.cpp

--- ejercicio1 heroe.cpp
+++ ejercicio1 heroe.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void mostrarEstado ("Arthas", int hp = 100, int maxHP = 100){
+    
+    int main(){
+int hp = x, HP = y 
+int x=100, y=100
+
+
+cout << ===Estado del Héroe << HP: X/Y << y << endl ;
+cout << Arthas << HP: X/Y << y << endl ;
 
